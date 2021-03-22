5eca365d0c91533c2279492876317a8a7aa58280
Wang David <wangdavid@WangdeMacBook-Pro.local>
2021-03-22 10:20:35 +0800

    add

---
+++ Week_09/src/com/david/leetcode/IsMatch_10.java
@@ -0,0 +1,15 @@
+package com.david.leetcode;
+
+public class IsMatch_10 {
+
+    public static void main(String[] args) {
+        System.out.println(new IsMatch_10().new Solution().isMatch("aa", "a"));
+    }
+
+    //TODO
+    class Solution {
+        public boolean isMatch(String s, String p) {
+            return false;
+        }
+    }
+}
